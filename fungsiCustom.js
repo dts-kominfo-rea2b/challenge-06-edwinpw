9f0a433cd7b96c49890b29e79c27c1a0f1da4b03
Edwin Prastyan <edwinprastyan88@gmail.com>
2022-06-11 21:22:30 +0700

    update

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -18,7 +18,72 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const fs = require('fs');
+
+const getSecondWord = (words) => {
+
+  const word = words.split(' ');
+  console.log(word[1]);
+
+}
+
+const readJsonData = (dataJson) => {
+  const data = JSON.parse(dataJson)
+  let word = ''
+
+  // case object
+  if (data?.message !== undefined) {
+    word = data?.message
+  }
+
+  // case array
+  if (data?.length) {
+    data?.forEach(item => {
+      
+      if (item?.message !== undefined) {
+        word = item?.message
+      }
+
+      if (item?.data?.message !== undefined) {
+        word = item?.data?.message
+      }
+    })
+  }
+
+  return getSecondWord(word)
+}
+
+
+const bacaData = (fnCallback) => {
+
+  const hasilAkhir = [];
+
+  fs.readFile(file1, 'utf-8', (err, data) => {
+    if (err) {
+      return console.log('Ada terjadi error: ' + err);
+    }
+
+    hasilAkhir.push(readJsonData(data));
+
+    fs.readFile(file2, 'utf-8', (err, data) => {
+      if (err) {
+        return console.log('Ada terjadi error: ' + err);
+      }
+      hasilAkhir.push(readJsonData(data));
+
+      //Membaca file ke 3
+      fs.readFile(file3, 'utf-8', (err, data) => {
+        if (err) {
+          return console.log('Ada terjadi error: ' + err);
+        }
+        hasilAkhir.push(readJsonData(data));
+
+        return fnCallback(err, hasilAkhir);
+      })
+    })
+  })
+}
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
